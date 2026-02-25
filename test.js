290ca80a5538a138582cba0caa93301a17c40e23
Sakibul Islam Shadhin <100869344+Shadhinbd@users.noreply.github.com>
2026-02-25 12:20:24 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,125 @@
+// const student ={
+// name: "rafi",
+// age: 20 ,
+// };
+// student["name"] = "Rahul";
+
+// for(let num=0; num<=100; num++){
+// if (num)
+// console.log("num = ", num);
+// }
+
+// let game = 25 ; 
+// let user = prompt("Guess the number");
+// while (user != game){
+//  user = prompt("YOU entered wrong number");
+// }
+
+// console.log("congratulations");
+
+// console.log("hi \ nthere");
+
+
+// let name = "  Shad  hin    ";
+// let newt = name.toUpperCase();
+// console.log(newt);
+// console.log(name.toLowerCase());
+// let news = name.trim();
+// console.log(news);
+
+// let str = "name :";
+// let name = "shadin";
+// console.log (str.concat(name));
+
+// let marks = [10,20,30,40,50];
+
+// console.log(marks);
+
+// console.log(marks[2]);
+
+// let length = marks.length ;
+
+// console.log(length);
+
+// let name = ['sadhin' , "rafi" , "sakib" , "islam"];
+
+// for (let i=0; i < name.length; i++ ){
+ 
+//   console.log(name[i]);
+// }
+
+
+
+// let marks = [85,97,44,37,76,60];
+// let sum = 0  ;
+
+// for (let val of marks)
+// {
+//   sum += val ; 
+// }
+// let avg = sum/marks.length;
+// console.log("the average : ", avg);
+
+
+// let items = [250, 645, 300, 900,50];
+
+// for(let i = 0 ; i<items.length; i++){
+//   let offer = items[i] / 10 ;
+//   items[i] -= offer;
+// }
+// console.log(items);
+
+// let arr = [1,2,3,4,5,7];
+
+// //select from where>delete how many>insert what 
+// arr.splice(2,2, 1002,1234);
+
+// console.log(arr);
+
+// function vowels(str) {
+//   let count = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase(); 
+ 
+//     if (char === "a" || 
+//         char === "e" ||
+//         char === "i" || 
+//         char === "o" || 
+//         char === "u") {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+// Arrow Function
+
+// let mul = (a,b) => {
+//     console.log( a * b) ;
+// };
+
+// let hello = () =>{
+//     console.log("Printing hello");
+// }
+
+// let str = document.getElementsByClassName("hi");
+// console.dir(str);
+// console.log(str);
+
+let element = document.querySelector(".hi");
+console.dir(element);
+
+console.log("---------");
+
+
+let all = document.querySelectorAll(".hi");
+console.dir(all);
+console.log(all);
+
+
+
+
+
+
+
